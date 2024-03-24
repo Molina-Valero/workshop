ca7293a66bf6606d7694d169ff7241c857857c59
Juan Alberto Molina-Valero <52129566+Molina-Valero@users.noreply.github.com>
2024-03-24 11:39:13 +0100

    Add files via upload

---
+++ linReg.R
@@ -0,0 +1,34 @@
+#' Linear regression
+#'
+#' This function fit a linear regression.
+#'
+#' @param data Data frame containing Y and X variables.
+#' @param save.result Logical parameter indicating if results will be saved.
+#' @return Fit a linear regression.
+#' @export
+#' @importFrom plotly plot_ly
+
+linReg <- function(data, save.result = NULL){
+
+  mod <- lm(data$x ~ data$y)
+
+  print(summary(mod))
+
+  plot(data$y~data$x)
+  abline(mod)
+
+  # plotly::plot_ly(data, x = ~x, y = ~y, type = "scatter")
+
+  if (save.result){
+
+    out <- data.frame(beta_0 = coef(mod)[1],
+                      beta_1 = coef(mod)[2],
+                      R2 = summary(mod)$adj.r.squared)
+
+    write.csv(out, "out.csv", row.names = FALSE)
+
+  }
+
+}
+
+
